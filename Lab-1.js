fce8020cba6b39edcb25356ebd4e49b193a1b8dc
waelsofttun <wsboxtunisia@gmail.com>
2024-06-11 14:25:29 +0100

    Create Lab-1.js
    variables (i.e. naming, declaring, initializing and modifying their values)

---
+++ Lab-1.js
@@ -0,0 +1,15 @@
+let Name_1 = "Maxwell Wright"
+let Name_2 = "Raja Villarreal"
+let Name_3 = "Helen Richards"
+
+let Phone_1 = "(0191) 719 6495"
+let Phone_2 = "0866 398 2895 "
+let Phone_3 = "0800 1111"
+
+
+let email_1="Curabitur.egestas.nunc@nonummyac.co.uk"
+let email_2="posuere.vulputate@sed.com"
+let email_3="libero@convallis.edu"
+
+console.log( Name_1 , Phone_1 ,email_1) ;
+console.log( Name_3 , Phone_3 ,email_3) ;
