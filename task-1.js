f6dc33174ad01cfa3b1a731374f902f9e2df4849
waelsofttun <wsboxtunisia@gmail.com>
2024-06-11 14:13:54 +0100

    Create task-1.js
    Variable declaration Task

---
+++ task-1.js
@@ -0,0 +1,28 @@
+const RosePrice=8
+const LilyPrice=10
+const TulipPrice=2
+
+let RoseQt=70
+let LilyQt=50
+let TulipQt=120
+
+let TotalRose = RosePrice * RoseQt
+let TotalLily = LilyPrice * LilyQt
+let TotalTulip = TulipPrice * TulipQt
+
+console.log("Rose – unit price: "+ RosePrice , "quantity: " + RoseQt ," value: " + TotalRose);
+console.log("Lily  – unit price: "+ LilyPrice , "quantity: " + LilyQt ," value: " + TotalLily);
+console.log("Tulip  – unit price: "+ TulipPrice , "quantity: " + TulipQt ," value: " + TotalTulip);
+console.log("Total : " ,TotalRose+TotalLily+TotalTulip);
+
+RoseQt =RoseQt - 20;
+LilyQt = LilyQt - 30;
+
+ TotalRose = RosePrice * RoseQt
+ TotalLily = LilyPrice * LilyQt
+ TotalTulip = TulipPrice * TulipQt
+
+console.log("Rose – unit price: "+ RosePrice , "quantity: " + RoseQt ," value: " + TotalRose);
+console.log("Lily  – unit price: "+ LilyPrice , "quantity: " + LilyQt ," value: " + TotalLily);
+console.log("Tulip  – unit price: "+ TulipPrice , "quantity: " + TulipQt ," value: " + TotalTulip);
+console.log("Total : " ,TotalRose+TotalLily+TotalTulip);
