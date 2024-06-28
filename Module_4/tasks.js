9fe918cec44ef7f07e36263100dc2a24e4f0931c
waelsofttun <wsboxtunisia@gmail.com>
2024-06-28 14:20:24 +0100

    Create tasks.js
    Some tasks about loops

---
+++ Module_4/tasks.js
@@ -0,0 +1,88 @@
+let a=+prompt("from : " , 0);
+let b=+prompt("to   : ",100)
+
+if (a<b)
+for(let i=a ; i<=b ; i+=10)
+console.log(i);
+else 
+console.log("invalide")
+
+
+let movies = [];
+while (true) {
+    let title = prompt("Enter movie title");
+    let rating = prompt("Enter movie rating (imdb)");
+
+    if (title === null || rating === null) {
+        break
+    } else {
+        movies.push({
+            title: title,
+            rating: Number(rating)
+        });
+    }
+}
+
+console.log("All with ratings under 7:");
+for (movie of movies) {
+    if (movie.rating < 7) {
+        console.log('${movie.title} (${movie.rating})');
+    }
+}
+
+console.log("All with ratings over 7:");
+for (movie of movies) {
+    if (movie.rating >= 7) {
+        console.log('${movie.title} (${movie.rating})');
+        break;
+    }
+}
+
+let vessel = {
+    LATITUDE: 40.07288,
+    LONGITUDE: 154.48535,
+    COURSE: 285.6,
+    SPEED: 14.0,
+    IMO: 9175717,
+    NAME: "MARENO"
+}
+
+for (let key in vessel) {
+    console.log('${key} -> ${vessel[key]}');
+}
+
+while (true) {
+    let firstNumber = prompt("Enter first number");
+    let secondNumber = prompt("Enter second number");
+    let operand = prompt("Enter operand (+, -, * or /)");
+    let result;
+
+    if (firstNumber === "Q" || secondNumber === "Q" || operand === "Q") {
+        break;
+    }
+
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+
+    if (!Number.isNaN(firstNumber) && !Number.isNaN(secondNumber)) {
+        switch (operand) {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                result = firstNumber / secondNumber;
+                break;
+            default:
+                result = "Error: unknown operand";
+        }
+    } else {
+        result = "Error: at least one of the entered values is not a number";
+    }
+    alert(result);
+}
