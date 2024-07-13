db75a9f0049e279d74237542d33cca548e8d9018
waelsofttun <wsboxtunisia@gmail.com>
2024-07-13 12:18:26 +0100

    Update labs.js

--- Module_7/labs.js
+++ Module_7/labs.js
@@ -39,3 +39,39 @@ for(let x of paints){
 
 
 
+    //constructor
+let Image = function(title,artist,date){
+    this.title = title
+    this.artist = artist
+    this.date =date }
+    
+let images = {    
+    list : [],
+    contains(title){
+        return this.list.some(el=>el.title === title)},
+    add(title,artist,date){
+        if(!this.contains(title))
+        this.list.push( new Image(title,artist,date))},
+    show (){
+       
+        for(let x of this.list){
+         console.log("------")
+           for (let key in x)
+           console.log(`${key} : ${x[key]}`)}},
+    clear(){
+        this.list=[]
+        
+    }
+}   
+
+images.add('Mona Lisa', 'Leonardo da Vinci', 1503);
+images.add('The Last Supper', 'Leonardo da Vinci', 1495);
+images.add('The Starry Night', 'Vincent van Gogh', 1889);
+images.add('Mona Lisa', 'Leonardo da Vinci', 1503);
+images.show();
+// -> Mona Lisa (Leonardo da Vinci, 1503)
+// -> Last Supper (Leonardo da Vinci, 1495)
+// -> The Starry Night (Vincent van Gogh, 1889)
+images.clear();
+images.show();
+    
